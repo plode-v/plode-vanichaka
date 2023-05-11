9ba2e6b452589473c902aeec56e55b09bbb1fad9
Plode <84858705+plode-v@users.noreply.github.com>
2023-05-11 17:00:43 +0700

    merge branch 'new'

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,100 +4,38 @@ import { navItems } from '../constants'
 import "./index.css"
 
 const Navbar = () => {
-
-	const [showNav, setShowNav] = useState(false);
-	const [isOpen, setIsOpen] = useState(false);
-	const [scrollData, setScrollData] = useState({
-		y: 0,
-		lastY: 0
-	})
-
-	const toggleNavbar = () => {
-		setIsOpen(!isOpen);
-	};
-
-	const handleResumeDownload = () => {
-		null
-	}
-
-	useEffect(() => {
-		const handleScroll = () => {
-			setScrollData(prevState => {
-				return {
-					y: window.scrollY,
-					lastY: prevState.y
-				}
-			})
-		}
-		window.addEventListener("scroll", handleScroll);
-		return () => window.removeEventListener("scroll", handleScroll)
-		
-		
-	},[])
-
-	useEffect(() => {
-		if (scrollData.lastY < scrollData.y) {
-			setShowNav(true)
-		} else {
-			setShowNav(false);
-		}
-		
-	}, [scrollData])
-	
-
-
-
 	return (
-		<>
-			<Transition
-				show={isOpen}
-				enter="transition ease-out duration-200 transform origin-top-right"
-				enterFrom="opacity-0 scale-95 translate-x-full"
-				enterTo="opacity-100 scale-100"
-				leave="transition ease-in duration-150 transform origin-top-right"
-				leaveFrom="opacity-100 scale-100"
-				leaveTo="opacity-0 scale-95 translate-x-full"
-			>
-				<div className="fixed right-0 h-screen bg-[#F1F6F9] pt-20 sm:hidden">
-					<p className="text-black absolute top-2 right-4 font-epilogue text-[14px]" onClick={toggleNavbar}>close</p>
-					{navItems.map((item, index) => (
-						<a href={`#${item.sectionName}`} onClick={toggleNavbar} className="block py-1 px-8" key={index}>
-							<div className="flex lg:hidden rounded-lg w-[55vw] py-2 h-max justify-center">
-								<span className="font-medium text-black text-[20px]">{item.name}</span>
-							</div>
-						</a>
-					))}
-				</div>
-
-			</Transition>
-			<nav className={`bg-[#0F4C75] py-3 px-5 sm:py-4 sm:px-12 font-epilogue sticky top-0 transition-transform ease ${showNav && "-translate-y-full"}`}>
-				<div className="flex items-center justify-between">
-						<h1 className="text-[24px] sm:text-[32px] font-bold flex-1 cursor-default text-#F1F6F9]">
-							<a href="#">Plode</a>
-						</h1>
-
-					<div className="lg:hidden">
-						<button
-							type="button"
-							className="focus:outline-none"
-							onClick={toggleNavbar}
-						>
-							<FiMenu className="h-6 w-6" />
-						</button>
+		<nav className="h-full bg-transparent text-[#f1f6f9] pt-[15vh] fixed flex flex-col w-1/3">
+			<div className="pl-[25%] items-start">
+				<h1 className="3xl:text-[56px] text-[50px] font-[700] leading-tight">Plode Vanichaka</h1>
+				<h4 className="pb-[20px] text-[20px] text-[#f1f6f9]">Web3 & Full-Stack Developer</h4>
+				<p className="text-[#9ba4b5]">Building the future of the web with passion and technology</p>
+			</div>
+			<div className="pt-[80px] pl-[25%]">
+				{navItems.map((item, index) => (
+					<div className="flex items-center cursor-pointer w-max text-[#9ba4b5] hover:text-[#f1f6f9] hover:translate-x-5 duration-150" key={index}>
+						<div className="w-[30px] bg-[#9ba4b5] h-[2px] mr-[10px]" />
+						<li className="list-none text-[18px]">
+							<a href={`#${item.sectionName}`}>{item.name}</a>
+						</li>
 					</div>
-
-					{navItems.map((item, index) => (
-						<div className="hidden lg:flex font-epilogue font-semibold text-[18px] px-3" key={index}>
-							<a href={`#${item.sectionName}`} className="hover:opacity-50 duration-100 text-[#F1F6F9]">{item.name}</a>
-						</div>
-					))}
-					<div className="hidden lg:flex font-epilogue font-semibold text-[18px] px-3 items-center justify-center">
-						<button className="hover:scale-110 duration-100 text-[#F1F6F9] py-3 px-4 rounded-full bg-[#3282B8]">Resume</button>
-					</div>
-				</div>
- 
-			</nav>
-		</>
+				))}
+			</div>
+			<div className="flex items-center justify-start gap-6 text-[32px] h-[50vh] w-full pl-[25%]">
+				<a href="https://www.github.com/plode-v" target="_blank">
+					<FiGithub className="social-icons" />
+				</a>
+				<a href="https://www.linkedin.com/in/plode-vanichaka" target="_blank">
+					<FiLinkedin className="linkedin-icon" />
+				</a>
+				<a href="mailto:vanichaka.plode@gmail.com" target="_blank">
+					<FiMail className="social-icons" />
+				</a>
+				<a href="" target="_blank">
+					<FiFileText className="social-icons" />
+				</a>
+			</div>
+		</nav>
 	)
 }
 
