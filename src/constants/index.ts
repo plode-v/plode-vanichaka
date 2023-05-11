74dc9392c9a63b53e28d11b785da70b0476f3f4f
Plode <84858705+plode-v@users.noreply.github.com>
2023-05-11 19:04:22 +0700

    Fix bugs on projects component

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -36,5 +36,12 @@ export const projects = [
         techStack: ["React", "Auth0", "ExpressJS", "Mongoose", "Stripe API"],
         image: "https://img.freepik.com/free-psd/landing-page-online-fashion-sale_23-2148585401.jpg",
         link: "replace with deployed link",
-    }
+    },
+    {
+        name: "ETH DEX",
+        desc: "Hello, I'm Plode Vanichaka, a Thai-American based in Bangkok, Thailand. I'm a passionate developer with a focus on web3 development, front-end development, and all things related to programming As an American citizen, I want to emphasize that I am open to remote work opportunities and comfortable adjusting my work hours to align with the employets time zone. With no visa or permit requirement, I offer employers the convenience and flexibility to collaborate seamlessly across borders.My journey as a programmer began in October 2022, where I embarked on a self-taught path after completing my studies as an art student. It was during this time that I discovered my true passion for programming and its endless creativity possibilities.I am dedicated to continuously learning and delivering high-quality solutions, and Im excited about the opportunity to contribute my skills to meaningful projects.",
+        techStack: ["React", "Moralis", "Web3", "Truffle", "Mocha", "Ganache"],
+        image: "https://uploads-ssl.webflow.com/617aa5e4225be2555942852c/623863c59d3f5e9069970257_ParaSwap%20screenshot%402x.png",
+        link: "replace with deployed link"
+    },
 ]
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -4,7 +4,7 @@ import { projects } from "../constants"
 const Projects = () => {
 
     return (
-        <div className="w-full flex flex-col 3xl:flex-row 3xl:flex-1 px-10 h-screen" id="projects-section">
+        <div className="w-full grid grid-cols-1 3xl:grid-cols-2 gap-4 px-10" id="projects-section">
             {projects.map((item, index) => (
                 <Project 
                     key={index}
