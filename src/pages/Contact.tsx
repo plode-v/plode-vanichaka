08c60d3d3744746fd022c83ea756164b9cfdb4c6
Plode <84858705+plode-v@users.noreply.github.com>
2023-05-12 16:18:12 +0700

    Update Contact section

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -4,7 +4,7 @@ const Contact = () => {
     return (
         <div className="h-screen mt-10 w-full lg:px-10" id="contact-me-section">
             <h1 className="uppercase font-[700] text-[14px] lg:text-[18px] mb-10">Contact</h1>
-            <Form></Form>
+            <Form />
         </div>
     )
 }
