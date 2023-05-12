faf3ee6b747859641d35442604a401633572590c
Plode <84858705+plode-v@users.noreply.github.com>
2023-05-12 11:33:23 +0700

    Update screen sizing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import "./index.css"
 
 const Navbar = () => {
 	return (
-		<nav className="lg:h-full bg-transparent text-[#f1f6f9] pt-[5vh] lg:pt-[15vh] lg:fixed flex flex-col lg:w-1/3 w-screen">
+		<nav className="lg:h-full bg-transparent text-[#f1f6f9] pt-[5vh] lg:pt-[15vh] lg:fixed flex flex-col lg:w-1/3">
 			<div className="lg:pl-[25%] items-start">
 				<h1 className="3xl:text-[56px] lg:text-[50px] text-[30px] font-[700] leading-tight"><a href="/">Plode Vanichaka</a></h1>
 				<h4 className="lg:pb-[20px] text-[20px] text-[#f1f6f9]">Web3 & Full-Stack Developer</h4>
