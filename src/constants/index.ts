bd096b0a34b48cd5678a61329849efe61af61145
Plode <84858705+plode-v@users.noreply.github.com>
2023-07-29 23:38:17 +0700

    Setup version2

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -57,13 +57,21 @@ export const motionItem = {
 }
 
 export const projects = [
+    // FIXME: Add omni-ai home page photo
+    {
+        name: "Omni-AI",
+        desc: "Introducing Omni-AI, the all-in-one AI services platform that revolutionizes your creative experience. With seamless access to ChatGPT, DALL-E, and Replicate AI, Omni-AI empowers you to explore the realms of conversation, image generation, music, and videos U+2013 all under one subscription. Built on Next.js and styled with Tailwind CSS, the platform offers lightning-fast performance and a modern interface. Clerk ensures secure authentication, while the subscription-based model simplifies your workflow.",
+        techStack: ["Nextjs", "Tailwind CSS", "Shadcn-ui", "Clerk Auth"],
+        image: "",
+        link: "https://www.omni-ai.vercel.app"
+    },
     {
         name: "EasyFit",
         desc: "EasyFit is a web-application for tracking daily calorie intake and achieving fitness goals. Built with React and ExpressJS, offers a seamless and intuitive experience. 'This is still under the development'. With EasyFit, you can effortlessly log your meals, monitor your calorie consumption, and make informed choices about your diet. The secure authentication system ensures that your data remains private and accessible only to you. Powered by MongoDB, EasyFit ensures efficient data management and scalability. Stay on track with your fitness journey with EasyFit's upcoming mobile application, which will provide the same convenience and functionality on-the-go.",
         techStack: ["React", "Express", "Mongoose", "MongoDB"],
         image: easyFit,
         link: "https://github.com/plode-v/easyfit.git"
-    }
+    },
 ]
 
 export const documentation = [
