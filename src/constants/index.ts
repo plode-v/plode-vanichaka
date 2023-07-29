383b7ebf20e971048aa3eb7fb5eefa53c563d380
Plode <84858705+plode-v@users.noreply.github.com>
2023-07-29 23:43:55 +0700

    Add Omni-AI and update resume

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -58,6 +58,13 @@ export const motionItem = {
 
 export const projects = [
     {
+        name: "Omni-AI",
+        desc: "Introducing Omni-AI, the all-in-one AI services platform that revolutionizes your creative experience. With seamless access to ChatGPT, DALL-E, and Replicate AI, Omni-AI empowers you to explore the realms of conversation, image generation, music, and videos – all under one subscription. Built on Next.js and styled with Tailwind CSS, the platform offers lightning-fast performance and a modern interface. Clerk ensures secure authentication, while the subscription-based model simplifies your workflow.",
+        techStack: ['Nextjs', "Tailwind CSS", "Shadcn-ui", "Clerk Auth"],
+        image: "",
+        link: "https://omni-ai.vercel.app"
+    },
+    {
         name: "EasyFit",
         desc: "EasyFit is a web-application for tracking daily calorie intake and achieving fitness goals. Built with React and ExpressJS, offers a seamless and intuitive experience. 'This is still under the development'. With EasyFit, you can effortlessly log your meals, monitor your calorie consumption, and make informed choices about your diet. The secure authentication system ensures that your data remains private and accessible only to you. Powered by MongoDB, EasyFit ensures efficient data management and scalability. Stay on track with your fitness journey with EasyFit's upcoming mobile application, which will provide the same convenience and functionality on-the-go.",
         techStack: ["React", "Express", "Mongoose", "MongoDB"],
