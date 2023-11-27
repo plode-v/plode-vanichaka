8bb82f12f95258ec3b8a97dae7bbee0af6d80cbe
Plode <84858705+plode-v@users.noreply.github.com>
2023-11-27 12:11:39 +0700

    update navbar font

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,12 +2,10 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='w-full h-[80px] text-[#fafafa] flex justify-center z-20 absolute'>
+    <div className='w-full h-[80px] text-[#fafafa] flex justify-center z-20 absolute font-poppins'>
       <div className='bg-transparent h-full 3xl:w-2/3 w-full flex items-center justify-between p-8'>
-        <div>
-          <p className='text-[18px]'>Plode Vanichaka</p>
-        </div>
-        <div className='flex gap-8 text-[18px]'>
+          <p className='text-[18px] font-[300]'>Plode Vanichaka</p>
+        <div className='flex gap-8 text-[18px] font-[100]'>
           <button>About</button>
           <button>Work</button>
           <button>Contact</button>
