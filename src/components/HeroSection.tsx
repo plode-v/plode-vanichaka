591c6ef6dc6b0aa5d35f8422280db1f40478aed3
Plode <84858705+plode-v@users.noreply.github.com>
2023-11-27 12:07:25 +0700

    add navbar

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -4,9 +4,12 @@ const HeroSection = () => {
   return (
     <div className='bg-[#131036] h-screen w-full text-white items-center flex justify-center flex-col'>
         <p>Insert Globe</p>
-        <div className='absolute bottom-[10%] left-1/2 -translate-x-1/2 flex items-end w-[95%]'>
-            <h1 className='text-[120px] leading-none'>Plode Vanichaka</h1>
-            <p>Freelance Designer & Developer</p>
+        <div className='absolute bottom-[10%] left-1/2 -translate-x-1/2 flex items-end w-[95%] justify-between'>
+            <h1 className='text-[120px] leading-none tracking-tight font-[600]'>Plode Vanichaka</h1>
+            <div className='uppercase flex gap-2 text-[20px] tracking-wider pb-2'>
+              <p className='font-[300]'>freelance</p>
+              <p className='font-[600]'>designer & developer</p>
+            </div>
         </div>
     </div>
   )
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='w-full h-[80px] text-[#fafafa] flex justify-center z-20 absolute'>
+      <div className='bg-transparent h-full 3xl:w-2/3 w-full flex items-center justify-between p-8'>
+        <div>
+          <p className='text-[18px]'>Plode Vanichaka</p>
+        </div>
+        <div className='flex gap-8 text-[18px]'>
+          <button>About</button>
+          <button>Work</button>
+          <button>Contact</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
