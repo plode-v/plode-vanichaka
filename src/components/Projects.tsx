4ad5ff45e509f95fba2a1cfbfe09591dea76a1bc
Plode <84858705+plode-v@users.noreply.github.com>
2023-12-11 19:21:16 +0700

    Migrate all external data into utils/data.ts

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,35 +1,17 @@
 'use client'
-import React, { MouseEvent, useState, useEffect, useRef, MouseEventHandler } from 'react'
+import React from 'react'
 import ProjectComponent from './ProjectComponent'
+import { projects } from '@/utils/data'
 
 
 const Projects = () => {
-
-  const projectList = [
-    {
-      name: "vcs group",
-      role: "developer",
-      image: 'example 1'
-    },
-    {
-      name: 'ferworks security',
-      role: 'designer & developer',
-      image: 'example 2'
-    },
-    {
-      name: 'maison berger thailand',
-      role: 'teamlead developer',
-      image: 'example 3'
-    }
-
-  ]
 
   return (
     <div className={`min-h-screen w-full flex bg-neutral-100 justify-center`}>
         <div className='flex w-[95%] 3xl:w-2/3 justify-center duration-300'>
           <div className='w-full h-full'>
             <p className='text-[50px] 3xl:text-[65px] font-[700] py-12 opacity-20 uppercase'>projects</p>
-            {projectList.map((project, index) => (
+            {projects.map((project, index) => (
                 <ProjectComponent
                   key={index}
                   name={project.name}
---
+++ src/utils/data.ts
@@ -0,0 +1,17 @@
+export const projects = [
+    {
+        name: "vcs group",
+        role: 'developer',
+        image: 'example 1'
+    },
+    {
+        name: 'ferworks security',
+        role: 'designer & developer',
+        image: 'example 2',
+    },
+    {
+        name: 'maison berger thailand',
+        role: 'developer & admin',
+        image: 'example 3'
+    }
+]
