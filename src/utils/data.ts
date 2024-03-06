7e5f1ce2c8e2e66011a6905fe68c3fd8a0860fd4
Plode <84858705+plode-v@users.noreply.github.com>
2024-03-06 15:24:48 +0700

    update data

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -2,16 +2,19 @@ export const projects = [
     {
         name: "vcs group",
         role: 'developer',
-        image: 'example 1'
+        image: 'example 1',
+        link: ''
     },
     {
         name: 'ferworks security',
         role: 'designer & developer',
         image: 'example 2',
+        link: ''
     },
     {
         name: 'maison berger thailand',
         role: 'developer & admin',
-        image: 'example 3'
+        image: 'example 3',
+        link: ''
     }
 ]
